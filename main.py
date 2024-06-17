027b5507538b6bdc26c075eb97680c21c48f0859
PuNcHMaDeDeV2321 <160447914+PuNcHMaDeDeV2321@users.noreply.github.com>
2024-06-17 16:58:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import http.server
+import ssl
 from colorama import init, Fore, Style
 
 init()
@@ -19,6 +21,22 @@ def user_menu(directories):
     for i, directory in enumerate(directories, start=1):
         print(f"{i}. {directory}")
 
+def serve_with_ssl(directory):
+    server_address = ('localhost', 8000)
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('server.pem')
+
+    os.chdir(directory)
+    handler = http.server.SimpleHTTPRequestHandler
+
+    # Crear el servidor SSL
+    with http.server.HTTPServer(server_address, handler) as httpd:
+        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+        print(f"Servidor HTTPS iniciado en https://{server_address[0]}:{server_address[1]}")
+        httpd.serve_forever()
+
 def initial():
     templates_path = os.path.join(os.path.dirname(__file__), "Templates")
     pages = os.listdir(templates_path)
@@ -32,15 +50,13 @@ def initial():
         page_path = os.path.join(templates_path, page_directory, "index.html")
 
         if os.path.exists(page_path):
-            print(f"La página {page_directory} está disponible en http://localhost:8000")
-            os.chdir(os.path.join(templates_path, page_directory))
-            print("Cambiando al directorio:", os.getcwd())
-            server_process = subprocess.Popen(["python", "-m", "http.server"])
-            server_process.wait()
+            print(f"La página {page_directory} está disponible en https://localhost:8000")
+            page_directory_full_path = os.path.join(templates_path, page_directory)
+            serve_with_ssl(page_directory_full_path)
         else:
-            print(f"No encontrada")
+            print("Página no encontrada")
     except (ValueError, IndexError):
-        print("Por favor, ingrese un numero valido.")
+        print("Por favor, ingrese un número válido.")
 
 if __name__ == "__main__":
     initial()
